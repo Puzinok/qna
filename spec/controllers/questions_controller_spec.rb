2f5ce258a07548640807e6a0bd9c87315d9563bf
Pavel Puzin <puzinok@gmail.com>
2017-06-12 00:29:29 +0300

    test Question new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+  
+  describe 'GET #new' do
+    before { get :new }
 
+    it 'assigns new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :answer do
-    body ""
+    body "MyText"
     question nil
   end
 end
