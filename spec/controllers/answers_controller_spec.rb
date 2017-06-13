eb780b07ed5b47e4d40c597097878dfdf1bc4d98
Pavel Puzin <puzinok@gmail.com>
2017-06-14 01:27:25 +0300

    answer #new test

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer, question: question) }
+  
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+    
+    it 'assing answer @question to question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assings new Answer to new @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+  end
+end
