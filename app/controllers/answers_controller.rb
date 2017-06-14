78f14810538a79710c89b88888d81830ff93226b
Pavel Puzin <puzinok@gmail.com>
2017-06-14 17:22:46 +0300

    test valid Answer create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,24 @@
 class AnswersController < ApplicationController
-  def new
-    @question = Question.find(params[:question_id])    
+  before_action :set_question, only: [:new, :create]
+  
+  def new    
     @answer = @question.answers.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)    
+    if @answer.save
+      redirect_to @answer
+    end
+  end
+
+  private
+  
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+ 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save new answer in database' do
+        expect{ post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirect to show view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+  end
 end
