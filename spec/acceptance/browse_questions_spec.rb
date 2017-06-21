cb099d08b365dcb9fdfbbd93bd9c1ccc5fc3fc82
Pavel Puzin <puzinok@gmail.com>
2017-06-22 01:44:26 +0300

    fix browse all question feature

--- spec/acceptance/browse_questions_spec.rb
+++ spec/acceptance/browse_questions_spec.rb
@@ -5,9 +5,10 @@ feature 'Browse all questions', %q{
   As an user
   I want to be able browse all questions
 } do
+  given(:user) { create(:user) }
 
   scenario 'User can browse all questions' do
-    @questions = create_list(:valid_question, 3)
+    @questions = create_list(:valid_question, 3, user: user )
     visit '/questions'
     expect(page).to have_css('.question_title', count: 3)
   end
