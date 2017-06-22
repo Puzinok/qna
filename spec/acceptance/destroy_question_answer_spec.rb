d80f35b378f0fb37cb7a55b68b54968601e12ba5
Pavel Puzin <puzinok@gmail.com>
2017-06-22 14:27:53 +0300

    finished User can delete his own questions

--- spec/acceptance/destroy_question_answer_spec.rb
+++ spec/acceptance/destroy_question_answer_spec.rb
@@ -6,19 +6,24 @@ feature 'User delete own question or answer', %q{
   I want to be able to delete own question or answer
 } do
   given(:user) { create(:user) }
+  given(:question) { create(:valid_question, user: user) }
+  given(:another_user) { create(:user) }
 
-  scenario 'User can delete own question' do
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+  scenario 'Author can delete the question' do
+    sign_in(user)
 
-    @question = Question.create!(title: 'Test title', body: 'Question body', user: :current_user)
-    visit question_path(@question)
-    click_on 'Delete'
-
-    expect(page).to have_content('Your question succefully deleted!')
+    visit question_path(question)
+    click_on 'Delete Question'
+    expect(page).to have_no_content(question.title)
   end
 
-  scenario 'User cannot delete not owned him question'
+  scenario 'Another user cannot delete question' do
+    sign_in(another_user)
+
+    visit question_path(question)
+    expect(page).to_not have_content('Delete Question')
+  end
+
+  scenario 'Author can delete own answer'
+  scenario 'Non Author of answer cannot delete answer'
 end
