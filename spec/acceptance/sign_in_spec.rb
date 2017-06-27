d2cd2fcbf67b10888aad90d3f5f6039a910e405d
Pavel Puzin <puzinok@gmail.com>
2017-06-27 15:12:09 +0300

    Add test to sign_in spec.

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -7,10 +7,16 @@ feature 'Singin in', %q{
 } do
 
   given(:user) { create(:user) }
+  given(:unregistered_user) { User.new(email: 'user@test.com', password: '12345678') }
 
   scenario 'Existing User try to sign in' do
     sign_in(user)
     expect(page).to have_content('Signed in successfully.')
+  end
+
+  scenario 'Non register user try to sign in' do
+    sign_in(unregistered_user)
+    expect(page).to have_content('Invalid Email or password.')
   end
 end
 
