11c9bc5854aa9fe4f5cfc2790bab627584c6431d
Pavel Puzin <puzinok@gmail.com>
2017-06-29 22:31:57 +0300

    split feature destroy into two

---
+++ spec/acceptance/destroy_answer_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'User delete own answer', %q{
+  In order to delete wrong answer
+  As an authenticate user
+  I want to be able to delete own answer
+} do
+  given(:answer) { create(:answer) }
+
+  context 'Authenticated user' do
+    given(:user) { create(:user) }
+    given(:user_answer) { create(:answer, user: user) }
+
+    scenario 'Author can delete answer' do
+      sign_in(user)
+
+      visit question_path(user_answer.question)
+      click_on 'Delete Answer'
+      expect(page).to have_no_content(user_answer.body)
+    end
+
+    scenario 'Non Author of answer cannot delete answer' do
+      sign_in(user)
+
+      visit question_path(answer.question)
+      expect(page).to have_no_link('Delete Answer')
+    end
+  end
+
+  context 'Non authenticated user' do
+    scenario 'doesnt delete answer' do
+      visit question_path(answer.question)
+      expect(page).to have_no_link('Delete Answer')
+    end
+  end
+end
---
+++ spec/acceptance/destroy_question_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'User delete own question', %q{
+  In order to delete wrong question
+  As an authenticate user
+  I want to be able to delete own question
+} do
+
+  given(:question) { create(:question) }
+
+  context 'Authenticated user' do
+    given(:user) { create(:user) }
+    given(:user_question) { create(:question, user: user) }
+
+    scenario 'Author can delete question' do
+      sign_in(user)
+
+      visit question_path(user_question)
+      click_on('Delete Question')
+      expect(page).to have_no_content(user_question.title)
+      expect(page).to have_no_content(user_question.body)
+    end
+
+    scenario 'Non author try delete question' do
+      sign_in(user)
+
+      visit question_path(question)
+      expect(page).to have_no_link('Delete Question')
+    end
+  end
+
+  context 'Non autherticated user' do
+    scenario 'doesnt delete question' do
+      visit question_path(question)
+      expect(page).to have_no_link('Delete Question')
+    end
+  end
+end
