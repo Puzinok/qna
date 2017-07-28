f15efdb8df5ef5ca30ab729797dd4eaf96b74ca5
Pavel Puzin <puzinok@gmail.com>
2017-07-28 17:40:39 +0300

    transaction for best

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,9 +5,10 @@ class Answer < ApplicationRecord
   validates :body, presence: true
 
   def toggle_best!
-    self.best = :true
-    previous_best_answer = question.answers.find_by(best: true)
-    previous_best_answer&.update_column :best, :false
-    save
+    transaction do
+      previous_best_answer = question.answers.find_by(best: true)
+      previous_best_answer&.update_column(:best, false)
+      self.update_column(:best, true)
+    end
   end
 end
