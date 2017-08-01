15c4e6b797f283626d9e64e9d49a9995d63bb8f5
Pavel Puzin <puzinok@gmail.com>
2017-08-01 03:28:23 +0300

    user can attach multiple files when create question

--- spec/acceptance/attach_files_to_question_spec.rb
+++ spec/acceptance/attach_files_to_question_spec.rb
@@ -9,7 +9,7 @@ feature 'Author can attach files to question', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
-  scenario 'Author can attach files when create question' do
+  scenario 'Author can attach file when create question' do
     sign_in(user)
     visit new_question_path
     fill_in 'Title', with: question.title
@@ -18,5 +18,27 @@ feature 'Author can attach files to question', %q{
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'Author can attach multiple files whan create question', js: true do
+    sign_in(user)
+    visit new_question_path
+    fill_in 'Title', with: question.title
+    fill_in 'Body', with: question.body
+
+    within all('.nested-fields').first do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'add file'
+
+    within all('.nested-fields').first do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/3/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
 end
