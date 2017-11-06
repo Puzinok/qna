e93fc08b497bc8a3f5d7e1a6c2a7536807dc9387
Pavel Puzin <puzinok@gmail.com>
2017-11-06 12:23:08 +0300

    add profiles/users

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -8,6 +8,10 @@ class Api::V1::ProfilesController < ApplicationController
     respond_with current_resource_owner
   end
 
+  def users
+    respond_with User.where.not(id: doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
   protected
 
   def current_resource_owner
