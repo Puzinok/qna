0c33f23894561d732abf5a010689a79cf5feb6d4
Pavel Puzin <puzinok@gmail.com>
2017-11-08 13:34:04 +0300

    create question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -8,4 +8,12 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   def show
     respond_with @question
   end
+
+  def create
+    respond_with current_resource_owner.questions.create(question_params)
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -5,8 +5,8 @@ describe 'Questions API' do
   let!(:questions) { create_list(:question, 2) }
   let!(:question) { questions.first }
   let!(:answer) { create(:answer, question: question) }
-  let!(:question_comments) { create_list(:comment, 2, commentable: question) }
-  let(:question_comment) { question_comments.first }
+  let!(:comments) { create_list(:comment, 2, commentable: question) }
+  let(:comment) { comments.first }
   let!(:attach) { create(:attachment, attachable: question) }
 
   describe 'GET /index' do
@@ -63,7 +63,7 @@ describe 'Questions API' do
 
       %w(id title body created_at updated_at).each do |attr|
         it "question object contains #{attr}" do
-          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("#{attr}")
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path(attr.to_s)
         end
       end
 
@@ -74,7 +74,7 @@ describe 'Questions API' do
 
         %w(id body created_at updated_at).each do |attr|
           it "object contains #{attr}" do
-            expect(response.body).to be_json_eql(question_comment.send(attr.to_sym).to_json).at_path("comments/0/#{attr}")
+            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("comments/0/#{attr}")
           end
         end
       end
@@ -86,8 +86,59 @@ describe 'Questions API' do
 
         %w(attachments created_at updated_at).each do |attr|
           it "object contains #{attr}" do
-            expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("#{attr}")
+            expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path(attr.to_s)
           end
+        end
+      end
+    end
+  end
+
+  describe 'POST /create' do
+    context 'unauthorized' do
+      it 'returns 401 status if no access_token' do
+        post '/api/v1/questions', params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if no access_token is invalid' do
+        post '/api/v1/questions', params: { access_token: '112233', format: :json }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      it 'returns status 201' do
+        post '/api/v1/questions', params: { format: :json,
+                                            question: attributes_for(:question),
+                                            access_token: access_token.token }
+        expect(response.status).to eq 201
+      end
+
+      it 'create question' do
+        expect {
+          post '/api/v1/questions', params: { format: :json,
+                                              question: attributes_for(:question),
+                                              access_token: access_token.token }
+        }.to change(Question, :count).by(1)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "returns question contains #{attr}" do
+          post '/api/v1/questions', params: { format: :json,
+                                              question: attributes_for(:question),
+                                              access_token: access_token.token }
+
+          expect(response.body).to be_json_eql(Question.last.send(attr.to_sym).to_json).at_path("#{attr}")
+        end
+      end
+
+      %w(title body).each do |attr|
+        it "return error if #{attr} blank " do
+          post '/api/v1/questions', params: { format: :json,
+                                            question: attributes_for(:invalid_question),
+                                            access_token: access_token.token }
+
+          expect(response.body).to be_json_eql("can't be blank".to_json).at_path("errors/#{attr}/0")
         end
       end
     end
