552bb6b6ac69181c0ea727b423e6c650d1257b81
Pavel Puzin <puzinok@gmail.com>
2017-11-08 14:31:50 +0300

    create answer

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -8,4 +8,13 @@ class Api::V1::AnswersController < Api::V1::BaseController
   def show
     respond_with Answer.find(params[:id])
   end
+
+  def create
+    @question = Question.find(params[:question_id])
+    respond_with @question.answers.create(answer_params.merge(user_id: current_resource_owner.id))
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -63,7 +63,7 @@ describe 'Answer API' do
 
       %w(id body created_at updated_at).each do |attr|
         it "answer object contains #{attr}" do
-          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("#{attr}")
+          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path(attr.to_s)
         end
       end
 
@@ -86,9 +86,58 @@ describe 'Answer API' do
 
         %w(attachments created_at updated_at).each do |attr|
           it "object contains #{attr}" do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("#{attr}")
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path(attr.to_s)
           end
         end
+      end
+    end
+  end
+
+  describe 'POST /create' do
+    context 'unauthorized' do
+      it 'returns 401 status if no access_token' do
+        post "/api/v1/questions/#{question.id}/answers", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if no access_token is invalid' do
+        post "/api/v1/questions/#{question.id}/answers", params: { access_token: '112233', format: :json }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      it 'returns status 201' do
+        post "/api/v1/questions/#{question.id}/answers", params: { format: :json,
+                                                                   answer: attributes_for(:answer),
+                                                                   access_token: access_token.token }
+        expect(response.status).to eq 201
+      end
+
+      it 'create answer' do
+        expect {
+          post "/api/v1/questions/#{question.id}/answers", params: { format: :json,
+                                                                     answer: attributes_for(:answer),
+                                                                     access_token: access_token.token }
+        }.to change(question.answers, :count).by(1)
+      end
+
+      %w(id body created_at updated_at).each do |attr|
+        it "returns answer contains #{attr}" do
+          post "/api/v1/questions/#{question.id}/answers", params: { format: :json,
+                                                                     answer: attributes_for(:answer),
+                                                                     access_token: access_token.token }
+
+          expect(response.body).to be_json_eql(Answer.last.send(attr.to_sym).to_json).at_path(attr.to_s)
+        end
+      end
+
+      it "return error if body blank " do
+        post "/api/v1/questions/#{question.id}/answers", params: {  format: :json,
+                                                                    answer: attributes_for(:invalid_answer),
+                                                                    access_token: access_token.token }
+
+        expect(response.body).to be_json_eql("can't be blank".to_json).at_path("errors/body/0")
       end
     end
   end
