a0e31b541631a8750a8847464c6bcac4da0d479c
Pavel Puzin <puzinok@gmail.com>
2017-11-28 01:48:01 +0300

    configurated ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://qna.puzin.me' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -88,4 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'qna.puzin.me', port: 80 }
 end
